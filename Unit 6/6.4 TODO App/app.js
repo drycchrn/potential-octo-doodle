8ecbf67b47132760cd0b70001271b97b44bf182a
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-01-12 14:31:48 -0600

    ESLint Changes
    Change order according to ESLint noted issues

--- Unit 6/6.4 TODO App/app.js
+++ Unit 6/6.4 TODO App/app.js
@@ -1,74 +1,32 @@
-function createTodo(numOfTodo, todoText, todoStatus) {
-  var li = createLi(numOfTodo, todoStatus);
-  var checkbox = createCheckbox(numOfTodo, todoStatus);
-  var span = createSpan(todoText);
-  var remButton = createRemButton(numOfTodo);
-
-  li.appendChild(checkbox);
-  li.appendChild(span);
-  li.appendChild(remButton);
-  return li;
-}
-
 function createLi(numOfTodoForLi, todoStatus) {
-  var newLi = document.createElement("li");
-  var numberedLi = "todo-" + numOfTodoForLi;
+  const newLi = document.createElement('li');
+  const numberedLi = `todo-${numOfTodoForLi}`;
 
-  newLi.setAttribute("id", numberedLi);
+  newLi.setAttribute('id', numberedLi);
 
   if (todoStatus) {
-    newLi.classList.add("completedTodo");
+    newLi.classList.add('completedTodo');
   }
 
   return newLi;
 }
 
-function createCheckbox(numOfTodoForCheck, todoStatus) {
-  var newCheckbox = document.createElement("input");
-  var numberedCheckbox = "toggleTodo-" + numOfTodoForCheck;
-
-  newCheckbox.setAttribute("type", "checkbox");
-  newCheckbox.setAttribute("id", numberedCheckbox);
-  newCheckbox.setAttribute("title", "Complete Todo");
-  newCheckbox.addEventListener("click", toggleTodo);
-
-  if (todoStatus) {
-    newCheckbox.checked = true;
-  }
-
-  return newCheckbox;
-}
-
 function createSpan(todoTextforSpan) {
-  var newSpan = document.createElement("span");
+  const newSpan = document.createElement('span');
 
   newSpan.innerHTML = todoTextforSpan;
 
   return newSpan;
 }
 
-function createRemButton(numOfTodoForRem) {
-  var newRemButton = document.createElement("button");
-  var numberedRemButton = "removeTodo-" + numOfTodoForRem;
-
-  newRemButton.classList.add("removeTodo");
-  newRemButton.setAttribute("type", "submit");
-  newRemButton.setAttribute("id", numberedRemButton);
-  newRemButton.setAttribute("title", "Remove Todo");
-  newRemButton.innerHTML = "🗑️";
-  newRemButton.addEventListener("click", removeTodo);
-
-  return newRemButton;
-}
-
 function getIdNum() {
-  var currentLiElements = document.getElementById("todoList").children;
-  var highestNum = 0;
+  const currentLiElements = document.getElementById('todoList').children;
+  let highestNum = 0;
 
-  for (var li of currentLiElements) {
-    var id = li.getAttribute("id");
-    var splitId = id.split("-")[1];
-    var idNum = Number(splitId);
+  for (const li of currentLiElements) {
+    const id = li.getAttribute('id');
+    const splitId = id.split('-')[1];
+    const idNum = Number(splitId);
 
     if (idNum > highestNum) {
       highestNum = idNum;
@@ -78,19 +36,88 @@ function getIdNum() {
   return highestNum + 1;
 }
 
+function remTodoFromLocalStorage(numOfTodo) {
+  localStorage.removeItem(`data-${numOfTodo}`);
+}
+
+function loadLocalStorage() {
+  const loadList = { ...localStorage };
+  const dataIds = Object.keys(loadList).sort();
+  for (const dataId of dataIds) {
+    const data = JSON.parse(loadList[dataId]);
+    addTodoToList(data.id, data.text, data.check);
+  }
+}
+
+function addTodoToLocalStorage(numOfTodo, todoText) {
+  const data = {
+    id: numOfTodo,
+    text: todoText,
+    check: false,
+  };
+
+  localStorage.setItem(`data-${numOfTodo}`, JSON.stringify(data));
+}
+
+function completedInLocalStorage(numOfTodo) {
+  const localStorageId = `data-${numOfTodo}`;
+  const numOfCheck = JSON.parse(localStorage.getItem(localStorageId));
+  numOfCheck.check = true;
+  localStorage.setItem(localStorageId, JSON.stringify(numOfCheck));
+}
+
+function incompleteInLocalStorage(numOfTodo) {
+  const localStorageId = `data-${numOfTodo}`;
+  const numOfCheck = JSON.parse(localStorage.getItem(localStorageId));
+  numOfCheck.check = false;
+  localStorage.setItem(localStorageId, JSON.stringify(numOfCheck));
+}
+
+function toggleTodo(event) {
+  const checkbox = event.target;
+  const { id } = checkbox;
+  const numOfTodo = id.split('-')[1];
+  const todo = document.getElementById(`todo-${numOfTodo}`);
+
+  if (checkbox.checked) {
+    todo.classList.add('completedTodo');
+    completedInLocalStorage(numOfTodo);
+  } else {
+    todo.classList.remove('completedTodo');
+    incompleteInLocalStorage(numOfTodo);
+  }
+}
+
+function createCheckbox(numOfTodoForCheck, todoStatus) {
+  const newCheckbox = document.createElement('input');
+  const numberedCheckbox = `toggleTodo-${numOfTodoForCheck}`;
+
+  newCheckbox.setAttribute('type', 'checkbox');
+  newCheckbox.setAttribute('id', numberedCheckbox);
+  newCheckbox.setAttribute('title', 'Complete Todo');
+  newCheckbox.addEventListener('click', toggleTodo);
+
+  if (todoStatus) {
+    newCheckbox.checked = true;
+  }
+
+  return newCheckbox;
+}
+
 function addTodoToList(numOfTodo, todoText, todoStatus) {
-  const ul = document.getElementById("todoList");
-  var createdTodo = createTodo(numOfTodo, todoText, todoStatus);
+  const ul = document.getElementById('todoList');
+  const createdTodo = createTodo(numOfTodo, todoText, todoStatus);
   ul.append(createdTodo);
 }
 
-function getTodoInputAndAddTodoToList(event) {
-  var todoText = document.getElementById("newTodo").value;
+function getTodoInputAndAddTodoToList() {
+  const todoText = document.getElementById('newTodo').value;
   if (todoText.length < 3) {
-    alert("Please input more text.");
+    const alert = 'Please input more text.';
+    alert();
   } else {
-    var numOfTodo = getIdNum();
-    document.getElementById("newTodo").value = "";
+    const numOfTodo = getIdNum();
+    document.getElementById('newTodo').value = '';
     addTodoToList(numOfTodo, todoText, false);
     addTodoToLocalStorage(numOfTodo, todoText);
   }
@@ -98,64 +125,38 @@ function getTodoInputAndAddTodoToList(event) {
   return false;
 }
 
-function toggleTodo(event) {
-  var checkbox = event.target;
-  var id = checkbox.id;
-  var numOfTodo = id.split("-")[1];
-  var todo = document.getElementById("todo-" + numOfTodo);
-
-  if (checkbox.checked) {
-    todo.classList.add("completedTodo");
-    completedInLocalStorage(numOfTodo);
-  } else {
-    todo.classList.remove("completedTodo");
-    incompleteInLocalStorage(numOfTodo);
-  }
-}
-
 function removeTodo(event) {
-  var trashcan = event.target;
-  var id = trashcan.id;
-  var numOfTodo = id.split("-")[1];
-  var todo = document.getElementById("todo-" + numOfTodo);
+  const trashcan = event.target;
+  const { id } = trashcan;
+  const numOfTodo = id.split('-')[1];
+  const todo = document.getElementById(`todo-${numOfTodo}`);
 
   todo.remove();
   remTodoFromLocalStorage(numOfTodo);
 }
 
-function remTodoFromLocalStorage(numOfTodo) {
-  localStorage.removeItem("data-" + numOfTodo);
+function createRemButton(numOfTodoForRem) {
+  const newRemButton = document.createElement('button');
+  const numberedRemButton = `removeTodo-${numOfTodoForRem}`;
+
+  newRemButton.classList.add('removeTodo');
+  newRemButton.setAttribute('type', 'submit');
+  newRemButton.setAttribute('id', numberedRemButton);
+  newRemButton.setAttribute('title', 'Remove Todo');
+  newRemButton.innerHTML = '🗑️';
+  newRemButton.addEventListener('click', removeTodo);
+
+  return newRemButton;
 }
 
-function loadLocalStorage() {
-  const loadList = { ...localStorage };
-  var dataIds = Object.keys(loadList).sort();
-  for (var dataId of dataIds) {
-    var data = JSON.parse(loadList[dataId]);
-    addTodoToList(data.id, data.text, data.check);
-  }
-}
+function createTodo(numOfTodo, todoText, todoStatus) {
+  const li = createLi(numOfTodo, todoStatus);
+  const checkbox = createCheckbox(numOfTodo, todoStatus);
+  const span = createSpan(todoText);
+  const remButton = createRemButton(numOfTodo);
 
-function addTodoToLocalStorage(numOfTodo, todoText) {
-  var data = {
-    id: numOfTodo,
-    text: todoText,
-    check: false,
-  };
-
-  localStorage.setItem("data-" + numOfTodo, JSON.stringify(data));
-}
-
-function completedInLocalStorage(numOfTodo) {
-  var localStorageId = "data-" + numOfTodo;
-  var numOfCheck = JSON.parse(localStorage.getItem(localStorageId));
-  numOfCheck.check = true;
-  localStorage.setItem(localStorageId, JSON.stringify(numOfCheck));
-}
-
-function incompleteInLocalStorage(numOfTodo) {
-  var localStorageId = "data-" + numOfTodo;
-  var numOfCheck = JSON.parse(localStorage.getItem(localStorageId));
-  numOfCheck.check = false;
-  localStorage.setItem(localStorageId, JSON.stringify(numOfCheck));
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(remButton);
+  return li;
 }
