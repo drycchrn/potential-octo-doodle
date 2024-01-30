4ce7741167c975c02994c7f0bde851e0b6fbcc23
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-01-29 19:38:53 -0600

    WIP Memory Game
    Score Counter

--- Unit 6/6.5 Memory Game Challenge/script.js
+++ Unit 6/6.5 Memory Game Challenge/script.js
@@ -43,10 +43,6 @@ let shuffledColors = shuffle(colors);
 // it also adds an event listener for a click for each card
 
 // TODO: Implement this function!
-function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log('you just clicked', event.target);
-}
 
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
@@ -62,7 +58,57 @@ function createDivsForColors(colorArray) {
   }
 }
 
+function currentScoreCounter() {
+  let currentScore = Number(localStorage.getItem('gameScore'));
+  const currentScoreNumDiv = document.getElementById('currentScoreNum');
+
+  currentScore += 1;
+  currentScoreNumDiv.innerHTML = currentScore;
+
+  localStorage.setItem('gameScore', currentScore);
+}
+
+function handleCardClick(event) {
+  // you can use event.target to see which element was clicked
+  const cardClicked = event.target;
+  if (cardClicked) {
+    console.log('you just clicked', cardClicked);
+    currentScoreCounter();
+  }
+}
+
+function compareCurrentAndBestScoreToLocalStorage() {
+  const currentScore = currentScoreCounter();
+  const bestScoreInLocalStorage = { bestScore: bestScore };
+  if (currentScore < bestScoreInLocalStorage) {
+    bestScoreInLocalStorage = currentScore;
+    localStorage.setItem(bestScore, JSON.stringify(bestScoreInLocalStorage));
+  }
+}
+
+function addBestScoreToLocalStorage() {
+  const bestScore = compareCurrentAndBestScoreToLocalStorage();
+  localStorage.removeItem('bestScore');
+  localStorage.setItem('bestScore', bestScore);
+}
+
+function loadBestScoreFromLocalStorage() {
+  const bestScoreInLocalStorage = localStorage.getItem('bestScore');
+  if (bestScoreInLocalStorage) {
+    const bestScoreNumDiv = document.getElementById('bestScoreNum');
+    bestScoreNumDiv.innerHTML = bestScoreInLocalStorage;
+  }
+}
+
 // when the DOM loads
 function loadPage() {
   createDivsForColors(shuffledColors);
+  loadBestScoreFromLocalStorage();
+  localStorage.setItem('gameScore', 0);
 }
+
+/*
+if score when game ends is < best score, make score best score and store in local storage
+on click add class
+if card doesn't equal card, remove class otherwise keep class and don't allow second click
+*/
