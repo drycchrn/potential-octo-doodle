25f9e240376fee0fee664a8e9396fa6e9aca7b6f
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-02-13 17:29:12 -0600

    WIP Loan Calculator

--- Unit 8/8.1 Unit Testing/calculator/calculator-test.js
+++ Unit 8/8.1 Unit Testing/calculator/calculator-test.js
@@ -1,11 +1,26 @@
-
-it('should calculate the monthly rate correctly', function () {
-  // ...
+it('should calculate the monthly payment correctly', function () {
+  const values = {
+    amount: 25000,
+    years: 4,
+    rate: 3.2,
+  };
+  expect(calculateMonthlyPaymentBasedOn(values)).toEqual('$555.57');
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it('should return a result with 2 decimal places', function () {
+  const values = {
+    amount: 100034,
+    years: 10,
+    rate: 21,
+  };
+  expect(calculateMonthlyPaymentBasedOn(values)).toEqual('$2,000.00');
 });
 
-/// etc
+it('should handle high interest rates', function () {
+  const values = {
+    amount: 10000,
+    years: 5,
+    rate: 99.9,
+  };
+  expect(calculateMonthlyPaymentBasedOn(values)).toEqual('$839.42');
+});
--- Unit 8/8.1 Unit Testing/calculator/calculator.js
+++ Unit 8/8.1 Unit Testing/calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById("calc-form");
+window.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('calc-form');
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener('submit', function (e) {
       e.preventDefault();
       update();
     });
@@ -11,30 +11,61 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function getCurrentUIValues() {
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+    amount: +document.getElementById('loan-amount').value,
+    years: +document.getElementById('loan-years').value,
+    rate: +document.getElementById('loan-rate').value,
+  };
 }
 
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const initialValues = {
+    amount: 11002,
+    years: 20,
+    rate: 12.3,
+  };
+  const amountField = document.getElementById('loan-amount');
+  const yearsField = document.getElementById('loan-years');
+  const rateField = document.getElementById('loan-rate');
+
+  amountField.value = initialValues.amount;
+  yearsField.value = initialValues.years;
+  rateField.value = initialValues.rate;
+  update();
 }
 
-// Get the current values from the UI
-// Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  updateMonthly(values);
 }
 
-// Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
+function calculateMonthlyPaymentBasedOn(values) {
+  const principleAmount = values.amount;
+  const monthlyInterestRate = values.rate / (100 * 12); //rate input was for yearly
+  const numOfMonthlyPayments = values.years * 12;
+  let calculatedMonthlyPayment = 0;
+
+  //monthly payment formula
+  calculatedMonthlyPayment =
+    (principleAmount * monthlyInterestRate) /
+    (1 - Math.pow(1 + monthlyInterestRate, -numOfMonthlyPayments));
+
+  //format monthly payment as string in USD
+  const formatting = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  };
+  const monthlyPayment = new Intl.NumberFormat('en-US', formatting);
+  const formattedMonthlyPaymentString = monthlyPayment.format(
+    calculatedMonthlyPayment
+  );
+
+  return formattedMonthlyPaymentString;
 }
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
-function updateMonthly(monthly) {
+function updateMonthly(values) {
+  const monthly = calculateMonthlyPaymentBasedOn(values);
+  const monthlyPaymentField = document.getElementById('monthly-payment');
+
+  monthlyPaymentField.innerHTML = monthly;
 }
