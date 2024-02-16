cf344df2ab5a27ecfe88a3e5cf3ef614d4abb2f5
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-02-16 16:14:11 -0600

    9.2 some and every Exercises Final

--- Unit 9/9.2 some and every Exercises/some-every.js
+++ Unit 9/9.2 some and every Exercises/some-every.js
@@ -1,3 +1,7 @@
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable max-len */
 /*
 Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise it returns false.
 
@@ -6,7 +10,11 @@ Examples:
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+  return arr.some(function (valueInArray) {
+    return valueInArray % 2 !== 0;
+  });
+}
 
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
@@ -16,17 +24,28 @@ Examples:
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+  return num
+    .toString()
+    .split('')
+    .some(function (valueInStringArray) {
+      return valueInStringArray === '0';
+    });
+}
 
 /*
-Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
+Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false.
 
 Examples:
     hasOnlyOddNumbers([1,3,5,7]) // true
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+  return arr.every(function (valueInArray) {
+    return valueInArray % 2 !== 0;
+  });
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -36,39 +55,50 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+  return arr.every(function (valueInArray) {
+    return arr.indexOf(valueInArray) === arr.lastIndexOf(valueInArray);
+  });
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
 
 Examples:
     var arr = [
-        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
-        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
-        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Elie', last:"Schoppik"},
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+        {title: "Instructor", first: 'Matt', last:"Lane"},
         {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
     ]
-    
+
     hasCertainKey(arr,'first') // true
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+  return arr.every(function (objectInArray) {
+    return key in objectInArray;
+  });
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
 
 Examples:
     var arr = [
-        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
-        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
-        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Elie', last:"Schoppik"},
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+        {title: "Instructor", first: 'Matt', last:"Lane"},
         {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
     ]
-    
+
     hasCertainValue(arr,'title','Instructor') // true
     hasCertainValue(arr,'first','Elie') // false
-    
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+  return arr.every(function (objectInArray) {
+    return objectInArray[key] === searchValue;
+  });
+}
