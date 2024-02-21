a7b3b7f27b3c6f2ac707260df7807d8e175b18ec
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-02-20 19:42:16 -0600

    9.3 WIP

--- Unit 9/9.3 reduce Exercises/reduce.js
+++ Unit 9/9.3 reduce Exercises/reduce.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /*
 Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
@@ -6,7 +7,15 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+  const nameToAddToArray = (accumulator, currentValue) => {
+    accumulator.push(currentValue[key]);
+
+    return accumulator;
+  };
+
+  return arr.reduce(nameToAddToArray, []);
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,45 +28,70 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+  const splitString = str.split('');
+  const numOfVowelsInString = (accumulator, currentLetter) => {
+    const vowels = 'aeiou';
+    const lowercaseLetter = currentLetter.toLowerCase();
+    const vowelInArray = vowels.indexOf(lowercaseLetter) !== -1;
+    if (vowelInArray) {
+      accumulator[lowercaseLetter]++;
+    } else {
+      accumulator[lowercaseLetter] = 1;
+    }
+
+    return accumulator;
+  };
+
+  return splitString.reduce(numOfVowelsInString, {});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
-    
-    addKeyAndValue(arr, 'title', 'Instructor') // 
+
+    addKeyAndValue(arr, 'title', 'Instructor') //
       [
-        {title: 'Instructor', name: 'Elie'}, 
-        {title: 'Instructor', name: 'Tim'}, 
-        {title: 'Instructor', name: 'Matt'}, 
+        {title: 'Instructor', name: 'Elie'},
+        {title: 'Instructor', name: 'Tim'},
+        {title: 'Instructor', name: 'Matt'},
         {title: 'Instructor', name: 'Colt'}
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+  const funFunc = (accumulator, currentName, index) => {
+    accumulator[index][key] = value;
+    return accumulator;
+  };
+
+  return arr.reduce(funFunc, arr);
+}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
 
 Examples:
-    
+
     function isEven(val){
         return val % 2 === 0;
     }
-    
+
     const arr = [1,2,3,4,5,6,7,8];
-    
+
     partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
-    
+
     function isLongerThanThreeCharacters(val){
         return val.length > 3;
     }
-    
+
     const names = ['Elie', 'Colt', 'Tim', 'Matt'];
-    
+
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+
+}
