64ef9f3ab51221586e1656ff2ff4d4d1f4f775e4
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-02-27 18:41:09 -0600

    11.3 - Arrow Functions Exercise

---
+++ Unit 11/11.3 Arrow Functions/11.3 Arrow Functions Exercise.js
@@ -0,0 +1,29 @@
+function double(arr) {
+  return arr.map(function (val) {
+    return val * 2;
+  });
+}
+
+/* Write an ES2015 Version */
+const double = (arr) => arr.map((val) => val * 2);
+
+
+function squareAndFindEvens(numbers) {
+  var squares = numbers.map(function (num) {
+    return num ** 2;
+  });
+  var evens = squares.filter(function (square) {
+    return square % 2 === 0;
+  });
+  return evens;
+}
+
+// initial refactor
+const squareAndFindEvens = numbers => {
+  var squares = numbers.map(num => num ** 2);
+  var evens = squares.filter(square => square % 2 === 0);
+  return evens;
+}
+
+// final refactor 
+const squareAndFindEvens = (numbers) => numbers.map((num) => num ** 2).filter((square) => square % 2 === 0);
