5900662ab8c4aa4eadc4c34924534b241ed353ac
drycchrn <118573343+drycchrn@users.noreply.github.com>
2024-02-27 18:40:55 -0600

    11.2 - let and const Exercise

---
+++ Unit 11/11.2 let and const/11.2 let and const Exercise.js
@@ -0,0 +1,18 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+/**
+ * Quiz 
+ * You can redeclare and reassign variables with var. Var scopes a variable to that function.
+ * You can reassign variable with let but cannot redeclare. Let  scopes the variable to the block.
+ * 
+ * You can redeclare and reassign variables with var. Var scopes a variable to that function.
+ * You cannot reassign or redeclare variables with const. Const scopes the variable to the block.
+ * 
+ * You can reassign variables with let but not const.
+ * 
+ * Hoisting is the behavior of moving variable declarations to the top of scope.
+ */
